b1720664bded8c8f7d7132255cac79c11c73546a
rahilvir <rahilvir.2003@gmail.com>
2025-02-14 14:41:09 -0800

    supabase connection and font

--- rottify/next.config.ts
+++ rottify/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: "https://dywpqjgdzayvkqsitqbv.supabase.co",
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR5d3BxamdkemF5dmtxc2l0cWJ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk1NjU0MTMsImV4cCI6MjA1NTE0MTQxM30.ljuYPefIEy8xI2BB2m6qbE5l8CTWPE7IhJTVcMDQolA",
+  },
 };
 
 export default nextConfig;
