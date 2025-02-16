860ae9745757d02694726e7884744be2b823f393
p-north <puneetnorth170@gmail.com>
2025-02-16 12:04:55 -0800

    removed old db keys

--- rottify/next.config.ts
+++ rottify/next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  env: {
-    NEXT_PUBLIC_SUPABASE_URL: "https://dywpqjgdzayvkqsitqbv.supabase.co",
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR5d3BxamdkemF5dmtxc2l0cWJ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk1NjU0MTMsImV4cCI6MjA1NTE0MTQxM30.ljuYPefIEy8xI2BB2m6qbE5l8CTWPE7IhJTVcMDQolA",
-  },
+
 };
 
 export default nextConfig;
