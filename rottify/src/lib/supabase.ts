6d2d91c173e7754a5de627ef0eef84e7ad3e5173
p-north <puneetnorth170@gmail.com>
2025-02-17 00:33:45 -0800

    update dockerfiles

--- rottify/src/lib/supabase.ts
+++ rottify/src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl:string = process.env.SUPABASE_URL!;
-const supabaseAnonKey:string = process.env.SUPABASE_KEY!;
+const supabaseUrl:string = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey:string = process.env.NEXT_PUBLIC_SUPABASE_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
